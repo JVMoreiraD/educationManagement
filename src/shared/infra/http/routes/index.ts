d0dcf19d51eef167395b4d089756284139bcc8a2
jvmoreirad <jvitormoreirad@hotmail.com>
2022-09-29 19:22:10 -0300

    feat: students routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import { schoolRoutes } from "./school.routes";
+import { studentsRoutes } from "./students.routes";
 import { teacherRoutes } from "./teachers.routes";
 
 const router = Router();
 
 router.use("/school", schoolRoutes);
 router.use("/teacher", teacherRoutes);
+router.use("/student", studentsRoutes);
 
 export { router };
