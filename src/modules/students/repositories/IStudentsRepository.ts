61f84d0b2af8750475d82706612f8db377110c16
jvmoreirad <jvitormoreirad@hotmail.com>
2022-09-29 18:15:31 -0300

    feat: Students repository

---
+++ src/modules/students/repositories/IStudentsRepository.ts
@@ -0,0 +1,15 @@
+import { Students } from "@prisma/client";
+
+import { ICreateStudentDTO } from "../dtos/ICreateStudentDTO";
+
+interface IStudentsRepository {
+    create({
+        id,
+        name,
+        registration,
+        schools_id,
+    }: ICreateStudentDTO): Promise<Students>;
+    listStudentsBySchool(schools_id: string): Promise<Students[]>;
+    deleteStudent(id: string): Promise<void>;
+}
+export { IStudentsRepository };
