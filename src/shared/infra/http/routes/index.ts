0c31aea19f97239a671579665018c9ef07f3ac3e
jvmoreirad <jvitormoreirad@hotmail.com>
2022-09-29 17:54:07 -0300

    feat: teacher routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { schoolRoutes } from "./school.routes";
+import { teacherRoutes } from "./teachers.routes";
 
 const router = Router();
 
 router.use("/school", schoolRoutes);
+router.use("/teacher", teacherRoutes);
 
 export { router };
---
+++ src/shared/infra/http/routes/teachers.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+import { CreateTeachersController } from "@modules/teachers/useCases/createTeachers/CreateTeacherController";
+import { DeleteTeacherController } from "@modules/teachers/useCases/deleteTeacher/DeleteTeacherController";
+import { ListAllTeachersController } from "@modules/teachers/useCases/listAllTeachers/ListAllTeachersController";
+import { ListTeachersBySchoolController } from "@modules/teachers/useCases/listTeachersBySchool/ListTeachersBySchoolController";
+
+const teacherRoutes = Router();
+
+const createTeachersController = new CreateTeachersController();
+const deleteTeacherController = new DeleteTeacherController();
+const listAllTeachersController = new ListAllTeachersController();
+const listTeachersBySchoolController = new ListTeachersBySchoolController();
+
+teacherRoutes.post("/", createTeachersController.handle);
+teacherRoutes.delete("/:id", deleteTeacherController.handle);
+teacherRoutes.get("/all", listAllTeachersController.handle);
+teacherRoutes.get("/:school_id", listTeachersBySchoolController.handle);
+
+export { teacherRoutes };
