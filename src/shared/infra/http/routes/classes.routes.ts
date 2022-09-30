9b96dd0cded6eb64a2166eae9a5b9c2b5bad45fb
jvmoreirad <jvitormoreirad@hotmail.com>
2022-09-30 19:33:49 -0300

    feat: new classes routes - get and delete

--- src/shared/infra/http/routes/classes.routes.ts
+++ src/shared/infra/http/routes/classes.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 
 import { CreateClassesController } from "@modules/classes/useCases/createClasses/CreateClassesController";
+import { DeleteClassesClassesController } from "@modules/classes/useCases/deleteClasses/DeleteClassesController";
+import { ListClassesClassesController } from "@modules/classes/useCases/listClassesBySchool/ListClassesBySchoolController";
 
 const classesRoutes = Router();
 
 const createClassesController = new CreateClassesController();
+const listClassesBySchoolController = new ListClassesClassesController();
+const deleteClassesController = new DeleteClassesClassesController();
 
 classesRoutes.post("/", createClassesController.handle);
+classesRoutes.get("/", listClassesBySchoolController.handle);
+classesRoutes.delete("/", deleteClassesController.handle);
 
 export { classesRoutes };
