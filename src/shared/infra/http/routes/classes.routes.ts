848ca7cf019e012716e02aef4e4f09d593bc5e4c
jvmoreirad <jvitormoreirad@hotmail.com>
2022-10-02 18:12:39 -0300

    feat: add new students routes

--- src/shared/infra/http/routes/classes.routes.ts
+++ src/shared/infra/http/routes/classes.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { AddNewStudentsController } from "@modules/classes/useCases/addNewStudents/AddNewStudentsController";
 import { CreateClassesController } from "@modules/classes/useCases/createClasses/CreateClassesController";
 import { DeleteClassesClassesController } from "@modules/classes/useCases/deleteClasses/DeleteClassesController";
 import { ListClassesClassesController } from "@modules/classes/useCases/listClassesBySchool/ListClassesBySchoolController";
@@ -9,9 +10,11 @@ const classesRoutes = Router();
 const createClassesController = new CreateClassesController();
 const listClassesBySchoolController = new ListClassesClassesController();
 const deleteClassesController = new DeleteClassesClassesController();
+const addNewStudentsController = new AddNewStudentsController();
 
 classesRoutes.post("/", createClassesController.handle);
 classesRoutes.get("/", listClassesBySchoolController.handle);
 classesRoutes.delete("/", deleteClassesController.handle);
+classesRoutes.put("/:classes_id", addNewStudentsController.handle);
 
 export { classesRoutes };
