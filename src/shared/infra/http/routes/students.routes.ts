e54491bf114254cc52815793039c6f103b2d59a1
jvmoreirad <jvitormoreirad@hotmail.com>
2022-10-03 09:49:18 -0300

    feat: update routes of students and teachers

--- src/shared/infra/http/routes/students.routes.ts
+++ src/shared/infra/http/routes/students.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ChangeStudentsNameController } from "@modules/students/useCases/changeStudentsName/ChangeStudentsNameController";
 import { CreateStudentController } from "@modules/students/useCases/createStudent/CreateStudentController";
 import { DeleteStudentController } from "@modules/students/useCases/deleteStudent/DeleteStudentController";
 import { ListStudentsBySchoolController } from "@modules/students/useCases/listStudentsBySchool/ListStudentsBySchoolController";
@@ -9,9 +10,11 @@ const studentsRoutes = Router();
 const createStudentController = new CreateStudentController();
 const listStudentsBySchoolController = new ListStudentsBySchoolController();
 const deleteStudentController = new DeleteStudentController();
+const changeStudentsNameController = new ChangeStudentsNameController();
 
 studentsRoutes.post("/", createStudentController.handle);
 studentsRoutes.get("/", listStudentsBySchoolController.handle);
 studentsRoutes.delete("/:id", deleteStudentController.handle);
+studentsRoutes.put("/", changeStudentsNameController.handle);
 
 export { studentsRoutes };
